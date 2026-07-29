f3bd3e5791683d0d815f233deaa99999e2ff4c74
dxf <dafydd@post.cymru>
2026-07-30 00:15:09 +0100

    Switch to in-built Whisper SRT writer after late night discovery

--- wesubanyvod.py
+++ wesubanyvod.py
@@ -14,6 +14,7 @@ torch.cuda.init()
 logging.info(f"Loaded CUDA device {config.cuda_device}")
 model = whisper.load_model(config.model).to(config.cuda_device) # Change this to your desired model
 logging.info(f"Loaded model {config.model}.")
+writer = whisper.utils.WriteSRT("static")
 
 # --- pick up directories from config ---
 input_directory = os.fsencode(config.input_folder)
@@ -23,25 +24,15 @@ logging.info(f"SRT output folder: {config.srt_folder}")
 
 def transcribe_audio(src_file):
     logging.warning(f"Transcribing {src_file}...")
-    transcribe = model.transcribe(audio=src_file,verbose=True)
-    with torch.cuda.device(config.cuda_device):
-        segments = transcribe['segments']
-        for segment in segments:
-            startTime = str(0)+str(timedelta(seconds=int(segment['start'])))+',000'
-            endTime = str(0)+str(timedelta(seconds=int(segment['end'])))+',000'
-            text = segment['text']
-            segmentId = segment['id']+1
-            segment = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if text[0] == ' ' else text}\n\n"
-            if config.c3vocmode:
-                srtFilename = config.srt_folder + '/' + f"{"-".join(src_file[src_file.find('/')+1:].split("-", 2)[:2])}.srt"
-            else:
-                srtFilename = config.srt_folder + '/' + src_file[src_file.find('/')+1:]
-
-            with open(srtFilename, 'a', encoding='utf-8') as srtFile:
-                srtFile.write(segment)
-    logging.info(f'Finished writing {srtFilename}')
-    return srtFilename
-
+    transcription = model.transcribe(audio=src_file,verbose=True)
+    with torch.cuda.device(config.cuda_device):  
+        if config.c3vocmode:
+            srtfilename = config.srt_folder + '/' + f"{"-".join(src_file[src_file.find('/')+1:].split("-", 2)[:2])}.srt"
+        else:
+            srtfilename = config.srt_folder + '/' + src_file[src_file.find('/')+1:]
+        with open(srtfilename, 'a', encoding='utf-8') as srtFile:
+            writer.write_result(transcription, srtFile)
+    logging.info(f'Finished writing {srtfilename}')
 for file in os.listdir(input_directory):
     src_file = os.fsdecode(file)
     if src_file.endswith(".mp3") or src_file.endswith(".wav") or src_file.endswith(".mp4") or src_file.endswith(".webm"): 
